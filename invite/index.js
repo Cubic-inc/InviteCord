65af4ca84c5b4ab73bca4d589008e17dcc420c9a
CoreByte <62032154+CoreBytee@users.noreply.github.com>
2021-12-11 15:24:52 +0100

    Update index.js

--- invite/index.js
+++ invite/index.js
@@ -30,7 +30,7 @@ window.onload = async () => {
 
         document.getElementById("image").src = "https://cdn.discordapp.com/icons/" + InviteData.guild.id + "/" + InviteData.guild.icon + ".png"
 
-        await Wait(2000)
+        await Wait(500)
         window.location = WidgedData.instant_invite
 
         console.log(WidgedData)
